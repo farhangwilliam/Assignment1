f9255a99a0fc600b9b34145d3a763aba38f302f7
william farhang <farhang.william@gmail.com>
2023-10-15 17:31:42 +0200

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by william farhang on 2023-10-15.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
